0f800ec194a092c2b4e567db7af091f26e84ad30
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-26 11:27:18 -0300

    nada

--- agorapods/src/components/Home.vue
+++ agorapods/src/components/Home.vue
@@ -5,51 +5,65 @@ import imageSrc from "../assets/imgs/card.png";
 
 <template>
   <main>
-    <div class="banners">
-      <img class="banner" src="../assets/imgs/banner.avif" alt="" />
-      <img class="imagem" src="../assets/imgs/image.jpg" alt="" />
-    </div>
-    <div class="cards">
-      <Card backgroundColor="Roxo" text="Descartáveis" :imageSrc="imageSrc" />
-      <Card backgroundColor="Azul" text="FreeBase" :imageSrc="imageSrc" />
-      <Card backgroundColor="Vermelho" text="Descartáveis" :imageSrc="imageSrc" />
-      <Card backgroundColor="Laranja" text="Descartáveis" :imageSrc="imageSrc" />
-      <Card backgroundColor="Verde" text="Descartáveis" :imageSrc="imageSrc" />
-    </div>
+    <section>
+      <div class="banners">
+        <img class="banner" src="../assets/imgs/banner.avif" alt="Banner principal" />
+        <img class="imagem" src="../assets/imgs/image.jpg" alt="Imagem lateral" />
+      </div>
+      <div class="cards">
+        <Card backgroundColor="Roxo" text="Descartáveis" :imageSrc="imageSrc" />
+        <Card backgroundColor="Laranja" text="FreeBase" :imageSrc="imageSrc" />
+        <Card backgroundColor="Vermelho" text="Descartáveis" :imageSrc="imageSrc" />
+        <Card backgroundColor="Laranja" text="Descartáveis" :imageSrc="imageSrc" />
+        <Card backgroundColor="Verde" text="Descartáveis" :imageSrc="imageSrc" />
+      </div>
+    </section>
   </main>
 </template>
 
 <style>
 main {
-  background-color: rgba(128, 128, 128, 0.178);
+  background-color: var(--gray-color);
   position: relative;
   left: 0;
   right: 0;
-  width: 100vw; /* Ocupa 100% da largura da viewport */
-  margin-left: calc(
-    (100% - 100vw) / 2
-  ); /* Centraliza o conteúdo considerando o max-width do contêiner */
+  width: 100vw;
+  margin-left: calc((100% - 100vw) / 2);
 }
 
-.cards{
+.cards {
   display: flex;
 }
 
 .imagem {
-  height: 500px;
+  width: 400px;
+  height: 400px;
+  aspect-ratio: 3 / 4;
+  object-fit: cover;
   border-radius: 1rem;
+  margin-left: 10px;
 }
 
 .banner {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 16 / 9;
+  object-fit: cover;
   border-radius: 1rem;
-  width: 1320px;
-  height: 500px;
 }
 
 .banners {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   margin-top: 30px;
-  padding: 25px;
+}
+
+section {
+  max-width: 1200px; 
+  width: 100%; 
+  position: absolute; 
+  left: 50%; 
+  transform: translateX(-50%); 
+  box-sizing: border-box; 
 }
 </style>
